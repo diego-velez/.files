559d8b4590b4c57c75c32231b24b15696b555695
Diego Velez <diego.velez.dev@gmail.com>
2024-12-23 10:26:56 -0400

    Leave mini.pick window at default position

--- .config/nvim/lua/dvt/plugins/mini.lua
+++ .config/nvim/lua/dvt/plugins/mini.lua
@@ -660,17 +660,6 @@ return { -- Collection of various small independent plugins/modules
       },
 
       window = {
-        config = function()
-          local height = math.floor(0.618 * vim.o.lines)
-          local width = math.floor(0.618 * vim.o.columns)
-          return {
-            anchor = 'NW',
-            height = height,
-            width = width,
-            row = math.floor(0.5 * (vim.o.lines - height)),
-            col = math.floor(0.5 * (vim.o.columns - width)),
-          }
-        end,
         prompt_prefix = '󰁔 ',
         prompt_cursor = ' 󰁍',
       },
