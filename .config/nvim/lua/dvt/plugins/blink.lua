0eef20840efc8c1b5151f2112d3e7acc8b68ae61
Diego Vélez <diego.velez.dev@gmail.com>
2024-12-29 19:15:49 -0400

    Go back to arrows for blink lmao

--- .config/nvim/lua/dvt/plugins/blink.lua
+++ .config/nvim/lua/dvt/plugins/blink.lua
@@ -28,18 +28,17 @@ return {
     ---@type blink.cmp.Config
     opts = {
       keymap = {
-        ['<C-CR>'] = { 'show', 'show_documentation' },
+        ['<C-CR>'] = { 'show', 'select_and_accept' },
         ['<C-e>'] = { 'hide_documentation', 'hide' },
-        ['<C-y>'] = { 'select_and_accept' },
 
-        ['<C-p>'] = { 'select_prev', 'fallback' },
-        ['<C-n>'] = { 'select_next', 'fallback' },
+        ['<Up>'] = { 'select_prev', 'fallback' },
+        ['<Down>'] = { 'select_next', 'fallback' },
+
+        ['<Tab>'] = { 'snippet_backward', 'show_documentation', 'fallback' },
+        ['<S-Tab>'] = { 'snippet_forward', 'fallback' },
 
         ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
         ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
-
-        ['<Tab>'] = { 'snippet_forward', 'fallback' },
-        ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
       },
 
       completion = {
