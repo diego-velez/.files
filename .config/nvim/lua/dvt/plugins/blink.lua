06748a672de06f4e48a0789150f2e7ec9cebc957
Diego Vélez <diego.velez.dev@gmail.com>
2024-12-29 13:47:33 -0400

    Use default keymaps for blink.cmp

--- .config/nvim/lua/dvt/plugins/blink.lua
+++ .config/nvim/lua/dvt/plugins/blink.lua
@@ -28,12 +28,18 @@ return {
     ---@type blink.cmp.Config
     opts = {
       keymap = {
-        ['<C-CR>'] = { 'show', 'select_and_accept', 'show_documentation' },
+        ['<C-CR>'] = { 'show', 'show_documentation' },
         ['<C-e>'] = { 'hide_documentation', 'hide' },
-        ['<UP>'] = { 'select_prev', 'snippet_backward', 'fallback' },
-        ['<DOWN>'] = { 'select_next', 'snippet_forward', 'fallback' },
-        ['<C-u>'] = { 'scroll_documentation_up', 'fallback' },
-        ['<C-d>'] = { 'scroll_documentation_down', 'fallback' },
+        ['<C-y>'] = { 'select_and_accept' },
+
+        ['<C-p>'] = { 'select_prev', 'fallback' },
+        ['<C-n>'] = { 'select_next', 'fallback' },
+
+        ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+        ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+
+        ['<Tab>'] = { 'snippet_forward', 'fallback' },
+        ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
       },
 
       completion = {
