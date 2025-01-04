ad7932c1e8f7c5f3fbb7b6ecb6cda23207f5d19b
Diego Velez <diego.velez.dev@gmail.com>
2025-01-04 09:27:06 -0400

    Use colorful-menu for blink.cmp

--- .config/nvim/lua/dvt/plugins/blink.lua
+++ .config/nvim/lua/dvt/plugins/blink.lua
@@ -19,7 +19,10 @@ return {
   {
     'saghen/blink.cmp',
     version = 'v0.*',
-    dependencies = 'rafamadriz/friendly-snippets',
+    dependencies = {
+      'rafamadriz/friendly-snippets',
+      'xzbdmw/colorful-menu.nvim',
+    },
     event = 'InsertEnter',
     -- allows extending the enabled_providers array elsewhere in your config
     -- without having to redefine it
@@ -47,6 +50,38 @@ return {
           draw = {
             treesitter = {
               'lsp',
+            },
+            components = {
+              label = {
+                width = { fill = true, max = 60 },
+                text = function(ctx)
+                  local highlights_info =
+                    require('colorful-menu').highlights(ctx.item, vim.bo.filetype)
+                  if highlights_info ~= nil then
+                    return highlights_info.text
+                  else
+                    return ctx.label
+                  end
+                end,
+                highlight = function(ctx)
+                  local highlights_info =
+                    require('colorful-menu').highlights(ctx.item, vim.bo.filetype)
+                  local highlights = {}
+                  if highlights_info ~= nil then
+                    for _, info in ipairs(highlights_info.highlights) do
+                      table.insert(highlights, {
+                        info.range[1],
+                        info.range[2],
+                        group = ctx.deprecated and 'BlinkCmpLabelDeprecated' or info[1],
+                      })
+                    end
+                  end
+                  for _, idx in ipairs(ctx.label_matched_indices) do
+                    table.insert(highlights, { idx, idx + 1, group = 'BlinkCmpLabelMatch' })
+                  end
+                  return highlights
+                end,
+              },
             },
           },
         },
