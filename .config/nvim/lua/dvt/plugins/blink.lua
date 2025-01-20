5241592d2a1f30d5e00cd46a17be7b5fdc06e397
Diego Vélez <diego.velez.dev@gmail.com>
2025-01-20 15:54:21 -0400

    Use luals 3rd library for luv
    As per kickstart

--- .config/nvim/lua/dvt/plugins/blink.lua
+++ .config/nvim/lua/dvt/plugins/blink.lua
@@ -5,13 +5,13 @@ return {
     'folke/lazydev.nvim',
     dependencies = {
       { 'justinsgithub/wezterm-types', lazy = true },
-      { 'Bilal2453/luvit-meta', lazy = true },
     },
     ft = 'lua',
+    cmd = 'LazyDev',
     opts = {
       library = {
         -- Load luvit types when the `vim.uv` word is found
-        { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
         { path = 'wezterm-types', mods = { 'wezterm' } },
       },
     },
