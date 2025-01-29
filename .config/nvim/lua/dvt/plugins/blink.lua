b6d2341e0112d5a788348c02b0932aeec31e3bd0
Diego Vélez <diego.velez.dev@gmail.com>
2025-01-29 10:45:18 -0400

    Update blink.cmp and use default keymaps

--- .config/nvim/lua/dvt/plugins/blink.lua
+++ .config/nvim/lua/dvt/plugins/blink.lua
@@ -32,17 +32,20 @@ return {
     opts = {
       keymap = {
         preset = 'none',
-        ['<C-CR>'] = { 'show', 'select_and_accept' },
-        ['<C-e>'] = { 'cancel', 'fallback' },
+        ['<C-CR>'] = { 'show', 'show_documentation', 'hide_documentation' },
+        ['<C-y>'] = { 'select_and_accept' },
+        ['<C-e>'] = { 'hide' },
 
-        ['<Up>'] = { 'select_prev', 'fallback' },
-        ['<Down>'] = { 'select_next', 'fallback' },
-
-        ['<Tab>'] = { 'snippet_forward', 'show_documentation', 'fallback' },
-        ['<S-Tab>'] = { 'snippet_backward', 'hide_documentation', 'fallback' },
+        ['<C-p>'] = { 'select_prev', 'fallback' },
+        ['<C-n>'] = { 'select_next', 'fallback' },
 
         ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
         ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+
+        ['<Tab>'] = { 'snippet_forward', 'fallback' },
+        ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+
+        ['<C-h>'] = { 'show_signature', 'hide_signature', 'fallback' },
       },
 
       completion = {
