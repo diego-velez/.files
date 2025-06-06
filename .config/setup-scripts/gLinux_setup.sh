b7241efe899df7f5855f8625fc10659fdc4490df
Diego Vélez <diego.velez.dev@gmail.com>
2025-06-06 20:18:52 +0000

    Update gLinux_setup.sh

--- .config/setup-scripts/gLinux_setup.sh
+++ .config/setup-scripts/gLinux_setup.sh
@@ -4,7 +4,7 @@
 
 echo "Installing programs"
 
-sudo apt install -y git vim fzf fish rofi ripgrep fd-find eza bat i3 i3blocks zoxide starship ninja-build gettext cmake curl build-essential
+sudo apt install -y git vim fzf fish rofi ripgrep fd-find eza bat i3 i3blocks zoxide starship ninja-build gettext cmake curl build-essential npm cargo python3-venv
 
 ### Follow go/i3#setting-up-chrome-remote-desktop ###
 
