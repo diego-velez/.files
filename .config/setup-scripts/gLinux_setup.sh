f84e2977bafc4de15e61e93ff881eaeabe3f113c
Diego Vélez <diego.velez.dev@gmail.com>
2025-06-06 15:27:34 +0000

    Update gLinux_setup.sh

--- .config/setup-scripts/gLinux_setup.sh
+++ .config/setup-scripts/gLinux_setup.sh
@@ -81,3 +81,9 @@ echo "Installing Wezterm"
 curl -LO https://github.com/wezterm/wezterm/releases/download/20240203-110809-5046fc22/wezterm-20240203-110809-5046fc22.Ubuntu22.04.deb
 
 sudo apt install -y ./wezterm-20240203-110809-5046fc22.Ubuntu22.04.deb
+
+### Reboot gLinux ###
+
+echo "DONE"
+
+sudo systemctl restart chrome-remote-desktop@diveto
