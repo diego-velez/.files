acd5bc08b1484ab8267e8c94dc5dfcdcd5041429
Diego Vélez <diveto@google.com>
2025-07-23 20:30:01 +0000

    refactor: Use debugprint only when in buffer where we have LSP

--- .config/nvim/lua/plugins/lsp_config.lua
+++ .config/nvim/lua/plugins/lsp_config.lua
@@ -18,6 +18,40 @@ return {
     'saghen/blink.cmp',
     'nvim-java/nvim-java',
     'saecki/live-rename.nvim',
+    {
+      'andrewferrier/debugprint.nvim',
+      ---@module "debugprint"
+      ---@type debugprint.GlobalOptions
+      opts = {
+        keymaps = {
+          normal = {
+            plain_below = '',
+            plain_above = '',
+            variable_below = '',
+            variable_above = '',
+            variable_below_alwaysprompt = '',
+            variable_above_alwaysprompt = '',
+            surround_plain = '',
+            surround_variable = '',
+            surround_variable_alwaysprompt = '',
+            textobj_below = '',
+            textobj_above = '',
+            textobj_surround = '',
+            toggle_comment_debug_prints = '',
+            delete_debug_prints = '',
+          },
+          insert = {
+            plain = '',
+            variable = '',
+          },
+          visual = {
+            variable_below = '',
+            variable_above = '',
+          },
+        },
+        display_counter = false,
+      },
+    },
   },
   config = function()
     -- vim.lsp.set_log_level 'off' WARN: this might be needed if getting lsp log too big ah type shi
@@ -25,62 +59,45 @@ return {
       group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
       callback = function(event)
         local map = function(keys, func, desc)
-          vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+          vim.keymap.set('n', keys, func, { buffer = event.buf, desc = desc })
         end
         local client = vim.lsp.get_client_by_id(event.data.client_id)
 
         map('gd', function()
           require('mini.pick').LspPicker('definition', true)
-        end, '[G]oto [D]efinition')
+        end, 'LSP: [G]oto [D]efinition')
 
         map('gr', function()
           require('mini.pick').LspPicker('references', true)
-        end, '[G]oto [R]eferences')
+        end, 'LSP: [G]oto [R]eferences')
 
         map('gI', function()
           require('mini.pick').LspPicker('implementation', true)
-        end, '[G]oto [I]mplementation')
+        end, 'LSP: [G]oto [I]mplementation')
 
         map('gy', function()
           require('mini.pick').LspPicker('type_definition', true)
-        end, '[G]oto T[y]pe Definition')
+        end, 'LSP: [G]oto T[y]pe Definition')
 
         map('gD', function()
           require('mini.pick').LspPicker('declaration', true)
-        end, '[G]oto [D]eclaration')
+        end, 'LSP: [G]oto [D]eclaration')
 
-        map('<leader>ca', vim.lsp.buf.code_action, 'Code [A]ction')
+        map('<leader>ca', vim.lsp.buf.code_action, 'LSP: Code [A]ction')
 
         map('<leader>cr', function()
           require('live-rename').rename { insert = true }
-        end, '[R]ename')
+        end, 'LSP: [R]ename')
 
-        -- Show hover with window
-        vim.api.nvim_create_autocmd({ 'VimEnter', 'VimResized' }, {
-          group = vim.api.nvim_create_augroup('DVT Hover', { clear = true }),
-          desc = 'Setup LSP hover window',
-          buffer = event.buf,
-          callback = function()
-            local width = math.floor(vim.o.columns * 0.8)
-            local height = math.floor(vim.o.lines * 0.3)
-
-            vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
-              border = 'rounded',
-              max_width = width,
-              max_height = height,
-            })
-          end,
-        })
-
-        map('h', vim.lsp.buf.hover, '[H]over')
+        map('h', vim.lsp.buf.hover, 'LSP: [H]over')
         vim.keymap.set('n', 'K', '<nop>')
 
         if
           client
           and client:supports_method(vim.lsp.protocol.Methods.textDocument_codeLens, event.buf)
         then
-          map('<leader>cc', vim.lsp.codelens.run, '[C]odelens')
-          map('<leader>cC', vim.lsp.codelens.refresh, 'Refresh [C]odelens')
+          map('<leader>cc', vim.lsp.codelens.run, 'LSP: [C]odelens')
+          map('<leader>cC', vim.lsp.codelens.refresh, 'LSP: Refresh [C]odelens')
         end
 
         if
@@ -95,8 +112,43 @@ return {
             else
               vim.notify('Inlay hints disabled', vim.log.levels.INFO)
             end
-          end, '[T]oggle [I]nlay Hints')
+          end, 'LSP: [T]oggle [I]nlay Hints')
         end
+
+        -- [H]ere aka we are *here* in the code
+        map('g?h', function()
+          require('debugprint').debugprint {}
+        end, 'We are [h]ere (below)')
+        map('g?H', function()
+          require('debugprint').debugprint { above = true }
+        end, 'We are [h]ere (above)')
+
+        -- [V]ariable aka this is the value of said variable
+        map('g?v', function()
+          require('debugprint').debugprint { variable = true }
+        end, 'This [v]ariable (below)')
+        map('g?V', function()
+          require('debugprint').debugprint { above = true, variable = true }
+        end, 'This [v]ariable (above)')
+
+        -- [P]rompt aka we want to see the value of user input variable
+        map('g?p', function()
+          require('debugprint').debugprint { variable = true, ignore_treesitter = true }
+        end, '[P]rompt for variable (below)')
+        map('g?P', function()
+          require('debugprint').debugprint { above = true, variable = true, ignore_treesitter = true }
+        end, '[P]rompt for variable (above)')
+
+        -- Other operations
+        map('g?d', function()
+          require('debugprint.printtag_operations').deleteprints()
+        end, '[D]elete all debugprint in current buffer')
+        map('g?t', function()
+          require('debugprint.printtag_operations').toggle_comment_debugprints()
+        end, '[T]oggle debugprint statements')
+        map('g?s', function()
+          MiniPick.builtin.grep({ pattern = 'DEBUGPRINT:' }, nil)
+        end, '[S]earch all debugprint statements')
       end,
     })
 
