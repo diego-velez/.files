56502df1c1a6f562e74d640c4a16217553d10c71
Diego Vélez <diego.velez.dev@gmail.com>
2025-07-31 20:17:14 +0000

    feat: Add focus.nvim plugin

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -40,3 +40,34 @@ vim.api.nvim_create_autocmd('BufReadPre', {
     vim.cmd 'let @/ = ""' -- Clear the search register using Vim command
   end,
 })
+
+-- Autocommands for focus.nvim
+local ignore_filetypes = {
+  'neo-tree',
+  'qf',
+  'minipick',
+  'minifiles',
+  'ministarter',
+  'OverseerListOutput',
+  'OverseerList',
+  'grug-far',
+}
+local ignore_buftypes = { 'nofile', 'prompt', 'popup' }
+
+local augroup = vim.api.nvim_create_augroup('FocusDisable', { clear = true })
+
+vim.api.nvim_create_autocmd('WinEnter', {
+  group = augroup,
+  callback = function(_)
+    vim.w.focus_disable = vim.tbl_contains(ignore_buftypes, vim.bo.buftype)
+  end,
+  desc = 'Disable focus autoresize for BufType',
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  group = augroup,
+  callback = function(_)
+    vim.w.focus_disable = vim.tbl_contains(ignore_filetypes, vim.bo.filetype)
+  end,
+  desc = 'Disable focus autoresize for FileType',
+})
