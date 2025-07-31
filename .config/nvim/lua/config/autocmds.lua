9fd7bd22ab327bcf6f161e5020605fa41f74fd90
Diego Vélez <diego.velez.dev@gmail.com>
2025-07-31 20:17:04 +0000

    feat: Add useful autocmds

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -28,3 +28,17 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.opt.formatoptions:remove { 'c', 'r', 'o' }
   end,
 })
+
+vim.api.nvim_create_autocmd('FocusGained', {
+  desc = 'Update file when it changes',
+  command = 'checktime',
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  desc = 'Enable wrap in these filetypes',
+  pattern = { 'gitcommit', 'markdown', 'text', 'log' },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+  end,
+})
