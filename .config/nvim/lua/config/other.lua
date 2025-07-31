e250ce445bf520a5c1d2dbf1e9ab78c0910d239a
Diego Vélez <diego.velez.dev@gmail.com>
2025-07-31 20:16:59 +0000

    fix: Do not hardcode home and bare dotfiles dir for Neovim

--- .config/nvim/lua/config/other.lua
+++ .config/nvim/lua/config/other.lua
@@ -22,12 +22,12 @@ local dotfiles_files = vim
     '--work-tree=' .. work_tree,
     'ls-files',
     '--full-name',
-    '/home/dvt',
+    home,
   }, { text = true })
   :wait()
 local cwd = vim.fn.getcwd()
 cwd = string.sub(cwd, #home + 2)
 if cwd ~= nil and string.find(dotfiles_files.stdout, cwd, 1, true) ~= nil then
-  vim.env.GIT_WORK_TREE = '/home/dvt'
-  vim.env.GIT_DIR = '/home/dvt/.files'
+  vim.env.GIT_WORK_TREE = home
+  vim.env.GIT_DIR = git_dir
 end
