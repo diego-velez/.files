37b64f7249f34eecc6d62ce1cec0236259291ccf
Diego Velez <diego.velez.dev@gmail.com>
2025-08-02 17:12:42 -0700

    refactor: Use TOML for custom color scheme
    https://wezterm.org/config/appearance.html#defining-a-color-scheme-in-a-separate-file

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -10,107 +10,10 @@ config.window_close_confirmation = "NeverPrompt"
 config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
 config.font_size = 11
 
-local dracula = {
-	foreground = "#F8F8F2",
-	background = "#282A36",
-	normal = {
-		black = "#21222C",
-		red = "#FF5555",
-		green = "#50FA7B",
-		yellow = "#F1FA8C",
-		blue = "#BD93F9",
-		magenta = "#FF79C6",
-		cyan = "#8BE9FD",
-		white = "#F8F8F2",
-	},
-	bright = {
-		black = "#6272A4",
-		red = "#FF6E6E",
-		green = "#69FF94",
-		yellow = "#FFFFA5",
-		blue = "#D6ACFF",
-		magenta = "#FF92DF",
-		cyan = "#A4FFFF",
-		white = "#FFFFFF",
-	},
-}
-
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 
-config.colors = {
-	foreground = dracula.foreground,
-	background = dracula.background,
-
-	cursor_bg = dracula.foreground,
-	cursor_fg = dracula.background,
-	cursor_border = dracula.foreground,
-
-	selection_bg = dracula.foreground,
-	selection_fg = dracula.background,
-
-	scrollbar_thumb = dracula.normal.blue,
-
-	split = dracula.normal.blue,
-
-	ansi = {
-		dracula.normal.black,
-		dracula.normal.red,
-		dracula.normal.green,
-		dracula.normal.yellow,
-		dracula.normal.blue,
-		dracula.normal.magenta,
-		dracula.normal.cyan,
-		dracula.normal.white,
-	},
-	brights = {
-		dracula.bright.black,
-		dracula.bright.red,
-		dracula.bright.green,
-		dracula.bright.yellow,
-		dracula.bright.blue,
-		dracula.bright.magenta,
-		dracula.bright.cyan,
-		dracula.bright.white,
-	},
-	tab_bar = {
-		background = "rgba(40, 42, 54, 85%)",
-
-		active_tab = {
-			bg_color = dracula.background,
-			fg_color = dracula.foreground,
-
-			intensity = "Bold",
-		},
-
-		inactive_tab = {
-			bg_color = "rgba(40, 42, 54, 85%)",
-			fg_color = dracula.foreground,
-
-			intensity = "Half",
-		},
-
-		inactive_tab_hover = {
-			bg_color = "rgba(40, 42, 54, 85%)",
-			fg_color = dracula.foreground,
-
-			intensity = "Half",
-			italic = true,
-			underline = "Single",
-		},
-
-		new_tab = {
-			bg_color = "rgba(40, 42, 54, 85%)",
-			fg_color = dracula.foreground,
-		},
-
-		new_tab_hover = {
-			bg_color = "rgba(40, 42, 54, 85%)",
-			fg_color = dracula.foreground,
-			intensity = "Bold",
-		},
-	},
-}
+config.color_scheme = "Dracula"
 config.inactive_pane_hsb = {
 	saturation = 1.0,
 	brightness = 0.8,
