7207368d50a56356a649596694579bef62a42729
Diego Vélez <diego.velez.dev@gmail.com>
2025-08-04 19:17:59 +0000

    refactor: Edit settings for markdown
    Google uses max line character of 80

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,6 +1,16 @@
+---@module "lazy"
+---@type LazyPluginSpec
 return {
   'MeanderingProgrammer/render-markdown.nvim',
   ft = 'markdown',
   dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' },
-  opts = {},
+  ---@module "render-markdown"
+  ---@type render.md.UserConfig
+  opts = {
+    on = {
+      render = function()
+        vim.opt_local.colorcolumn = '80'
+      end,
+    },
+  },
 }
