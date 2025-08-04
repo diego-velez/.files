74245df2008585784dfc11b4b07c61332c98e1ec
Diego Velez <diego.velez.dev@gmail.com>
2025-08-03 22:28:58 -0700

    feat: Add visual search keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -72,6 +72,22 @@ vim.keymap.set(
   { desc = "Rename using Vim's [S]ubstitution" }
 )
 
+-- Search visual selection
+vim.keymap.set(
+  'x',
+  '/',
+  [[y:let@/='\V'.escape(@",'/\\')<CR>nN]],
+  -- Lua way just in case
+  -- function()
+  --   vim.cmd.normal { '"zy', bang = true }
+  --
+  --   local text = vim.fn.getreg 'z'
+  --
+  --   vim.fn.setreg('/', '\\V' .. vim.fn.escape(text, '/\\'))
+  -- end,
+  { desc = 'Search visual selection' }
+)
+
 -- Commenting
 vim.keymap.set(
   'n',
