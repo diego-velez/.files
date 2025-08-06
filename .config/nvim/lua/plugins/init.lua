c69c8976caa7e9105af249ec32c8c0f4e637d582
Diego Vélez <diego.velez.dev@gmail.com>
2025-08-06 23:21:26 +0000

    feat: Add kulala plugin
    Allows making HTTP requests from Neovim :)

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -23,4 +23,23 @@ return {
       open_cmd = 'zen-browser %s --class typst-preview',
     },
   },
+  {
+    'mistweaverco/kulala.nvim',
+    --stylua: ignore
+    keys = {
+      {'<leader>r', '', desc = '[R]un'},
+      { '<leader>rs', function() require('kulala').run() end, ft = {'http', 'rest'}, desc = '[S]end request' },
+      { '<leader>ra', function() require('kulala').run_all() end, ft = {'http', 'rest'}, desc = 'Send [A]ll requests' },
+      { '<leader>rp', function() require('kulala').replay() end, ft = {'http', 'rest'}, desc = 'Run [P]revious' },
+      { '<leader>ru', function() require('kulala').toggle_view() end, ft = {'http', 'rest'}, desc = 'Toggle UI' },
+      { '{', function() require('kulala').jump_prev() end, ft = {'http', 'rest'}, desc = 'Previous Request' },
+      { '}', function() require('kulala').jump_next() end, ft = {'http', 'rest'}, desc = 'Next Request' },
+    },
+    ft = { 'http', 'rest' },
+    opts = {
+      global_keymaps = false,
+      global_keymaps_prefix = '',
+      kulala_keymaps_prefix = '',
+    },
+  },
 }
