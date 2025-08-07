537ec03570e8a4616f0d6000c0260e33204c8842
Diego Vélez <diego.velez.dev@gmail.com>
2025-08-07 17:47:48 +0000

    feat: Add q: UI to focus disabled types

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -53,6 +53,7 @@ local ignore_filetypes = {
   'grug-far',
   'undotree',
   'diff', -- undotree diff window
+  'vim', -- q:
 }
 local ignore_buftypes = { 'nofile', 'prompt', 'popup', 'nowrite' }
 
@@ -61,7 +62,7 @@ local augroup = vim.api.nvim_create_augroup('FocusDisable', { clear = true })
 vim.api.nvim_create_autocmd('WinEnter', {
   group = augroup,
   callback = function(_)
-    vim.b.focus_disable = vim.tbl_contains(ignore_buftypes, vim.bo.buftype)
+    vim.w.focus_disable = vim.tbl_contains(ignore_buftypes, vim.bo.buftype)
   end,
   desc = 'Disable focus autoresize for BufType',
 })
