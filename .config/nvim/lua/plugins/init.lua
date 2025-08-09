a3cdcd5334ef954c077da17ad001d3ab35aa71e4
Diego Vélez <diego.velez.dev@gmail.com>
2025-08-09 00:29:45 +0000

    feat: Setup rules for zen-mode

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -59,6 +59,33 @@ return {
           enabled = true,
         },
       },
+      on_open = function(win)
+        vim.schedule(function()
+          vim.g.mininotify_disable = true
+          MiniClue.disable_all_triggers()
+          MiniPick.config.window.old_config = MiniPick.config.window.config
+          MiniPick.config.window.config = function()
+            local height = vim.api.nvim_win_get_height(win)
+            local width = vim.api.nvim_win_get_width(win)
+            return {
+              relative = 'editor',
+              anchor = 'NW',
+              height = height,
+              width = width,
+              row = 0,
+              col = math.floor(vim.o.columns * 0.2) + 1,
+            }
+          end
+        end)
+      end,
+      on_close = function()
+        vim.schedule(function()
+          vim.g.mininotify_disable = false
+          MiniClue.enable_all_triggers()
+          MiniPick.config.window.config = MiniPick.config.window.old_config
+          MiniPick.config.window.old_config = nil
+        end)
+      end,
     },
   },
 }
