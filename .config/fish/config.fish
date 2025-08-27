b78ccec780d51ef022da23eb8b4700b9a28c4edd
Diego Velez <diego.velez.dev@gmail.com>
2025-08-27 12:34:28 -0400

    refactor: Extract fish_title function into its own file

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,31 +4,6 @@ if string match -q 'Debian' $os
     set is_glinux true
 end
 
-# Change title based on the last command ran
-#
-# If this function runs after a command, it still shows the previous command ran, instead of nothing.
-function fish_title
-    # Either use the current command ran, or the previous command
-    set -l command $argv
-    if test -z $argv; and not test -z $previous_command
-        set command $previous_command
-    else
-        set -g previous_command $argv
-    end
-
-    # If command has . for current directory, expand directory for title
-    if string match -q "*." $command
-        set command (string replace "." "$PWD" $command)
-    end
-
-    # Show the last command ran if there is any
-    if test -z $command
-        echo "$USER@$HOSTNAME:$PWD";
-    else
-        echo "$USER@$HOSTNAME:$PWD - $command";
-    end
-end
-
 # Disable welcome message
 set fish_greeting
 
