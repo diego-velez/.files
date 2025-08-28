348bc54af88a79dcaecbef9e9ce6fcab1901a712
Diego Velez <diego.velez.dev@gmail.com>
2025-08-28 15:40:29 -0400

    fix: Change mini.nvim URL to new location
    The mini.nvim repo changed location from echasnovski/mini.nvim to
    nvim-mini/mini.nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -180,7 +180,7 @@ end)
 later(function()
   add {
     source = 'MeanderingProgrammer/render-markdown.nvim',
-    depends = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' },
+    depends = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },
   }
 
   require('render-markdown').setup()
