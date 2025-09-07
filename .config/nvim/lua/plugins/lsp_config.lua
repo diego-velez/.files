e678ce2747c5286d1ecc84a8d71e5484b6952ddb
Diego Velez <diego.velez.dev@gmail.com>
2025-09-06 20:49:00 -0400

    feat: Use override LSP capabilities with MiniCompletion's

--- .config/nvim/lua/plugins/lsp_config.lua
+++ .config/nvim/lua/plugins/lsp_config.lua
@@ -158,8 +158,12 @@ local capabilities_override = {
     },
   },
 }
-local capabilities =
-  vim.tbl_deep_extend('force', vim.lsp.protocol.make_client_capabilities(), capabilities_override)
+local capabilities = vim.tbl_deep_extend(
+  'force',
+  vim.lsp.protocol.make_client_capabilities(),
+  MiniCompletion.get_lsp_capabilities(),
+  capabilities_override
+)
 
 local servers = {
   gopls = {
