df59aed0b354be9ac2865ecf4ce04058e21c4fb5
Diego Velez <diego.velez.dev@gmail.com>
2025-09-11 20:28:35 -0400

    fix: Add auto cmd groups to autocmds

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -2,15 +2,19 @@
 --  See `:help lua-guide-autocommands`
 
 -- Smart cursor line
+local smart_cursor = vim.api.nvim_create_augroup('Smart cursor', { clear = true })
 vim.api.nvim_create_autocmd('InsertLeave', {
+  group = smart_cursor,
   command = 'set cursorline',
 })
 
 vim.api.nvim_create_autocmd('InsertEnter', {
+  group = smart_cursor,
   command = 'set nocursorline',
 })
 
 vim.api.nvim_create_autocmd('FileType', {
+  group = vim.api.nvim_create_augroup('Disable new line comment', { clear = true }),
   desc = 'Disable new line comment',
   pattern = '*',
   callback = function()
@@ -19,11 +23,13 @@ vim.api.nvim_create_autocmd('FileType', {
 })
 
 vim.api.nvim_create_autocmd('FocusGained', {
+  group = vim.api.nvim_create_augroup('Update file on change', { clear = true }),
   desc = 'Update file when it changes',
   command = 'checktime',
 })
 
 vim.api.nvim_create_autocmd('FileType', {
+  group = vim.api.nvim_create_augroup('Enable wrap/spell', { clear = true }),
   desc = 'Enable wrap and spell in these filetypes',
   pattern = { 'gitcommit', 'markdown', 'text', 'log', 'typst' },
   callback = function()
@@ -33,6 +39,7 @@ vim.api.nvim_create_autocmd('FileType', {
 })
 
 vim.api.nvim_create_autocmd('BufReadPre', {
+  group = vim.api.nvim_create_augroup('Clear search', { clear = true }),
   desc = 'Clear the last used search pattern when opening a new buffer',
   pattern = '*',
   callback = function()
