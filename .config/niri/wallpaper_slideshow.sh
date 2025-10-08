cc0bd80928e5f9ba355ebc8f4360cf45ff699c4a
Diego Velez <diego.velez.dev@gmail.com>
2025-10-08 08:50:13 -0400

    feat: Remove unused wallpapers

---
+++ .config/niri/wallpaper_slideshow.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while true; do
+	# Change the wallpaper every 5 minutes
+	sleep 5m
+	swww img "$(find "$HOME/Pictures/Wallpapers/" -type f | shuf -n 1)" --transition-type any --transition-fps 120
+done
+
